0c782a4290d1cd5a0b7272a9adca2828d066ac0a
Chandana611 <110517872+Chandana611@users.noreply.github.com>
2024-06-20 12:18:08 +0530

    Add files via upload

---
+++ About.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react'
+export default class Introduction extends Component {
+  render() {
+    return (
+      <div>
+<div>
+<center>
+      <h1 className='h1'> 
+      E-Commerce Companies
+        </h1>
+    </center>
+<h2 className='abou'>Top five E-Commerce Companies</h2>
+ 
+<div className='head2'><h3>AMZ</h3></div>
+<div className='h3'>
+<h3>Company Name: AMZ</h3>
+<h3>categories: phone
+                Laptop
+</h3>
+<h3>Price: 2236</h3>
+<h3>Discount: 66</h3>
+<h3>rating: 4.5</h3>
+<h3>availability: yes</h3>
+</div>
+
+<div className='head2'><h2>FLP</h2></div>
+<div className='h3'>
+<h3>Company Name: FLP</h3>
+<h3>categories: Speaker,
+                Computer
+</h3>
+<h3>Price: 9099</h3>
+<h3>Discount: 80</h3>
+<h3>rating: 4.9</h3>
+<h3>availability: yes</h3>
+</div>
+                        
+<div className='head2'><h2>SNP</h2></div>
+<div className='h3'>
+<h3>Company Name: SNP</h3>
+<h3>categories: Tablet,
+                Earphone
+</h3>
+<h3>Price: 7780</h3>
+<h3>Discount: 32</h3>
+<h3>rating: 3.9</h3>
+<h3>availability: yes</h3>
+</div>                    
+
+<div className='head2'><h2>MYN</h2></div>
+<div className='h3'>
+<h3>Company Name: MYN</h3>
+<h3>categories: Bluetooth,
+                pendrive
+</h3>
+<h3>Price: 9790</h3>
+<h3>Discount: 52</h3>
+<h3>rating: 3.7</h3>
+<h3>availability: yes</h3>
+</div>    
+
+</div>
+</div>         
+    )
+  }
+}
